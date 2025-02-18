16cd96c0f29285f568f7443c727f60d8d7c44010
Milan Chudik <chudikm@gmail.com>
2025-02-18 22:23:41 +0100

    Initial commit of maven set up

---
+++ src/main/java/com/example/cloudtalk/CloudtalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudtalk/CloudtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
