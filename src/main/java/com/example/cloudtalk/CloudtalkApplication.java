a38adff33ed886f59a67636e706882d54aeb9701
Milan Chudik <chudikm@gmail.com>
2025-02-18 22:45:45 +0100

    Add checkstyle to enforce standards

--- src/main/java/com/example/cloudtalk/CloudtalkApplication.java
+++ src/main/java/com/example/cloudtalk/CloudtalkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudtalkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudtalkApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CloudtalkApplication.class, args);
+    }
+	
 }
