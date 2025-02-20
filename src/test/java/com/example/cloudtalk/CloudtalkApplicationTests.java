804c05d28dbc87f19de13caf1cd2ccc9f0d89bbc
Milan Chudik <chudikm@gmail.com>
2025-02-20 21:56:50 +0100

    Ignore test that require docker running

--- src/test/java/com/example/cloudtalk/CloudtalkApplicationTests.java
+++ src/test/java/com/example/cloudtalk/CloudtalkApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.cloudtalk;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled // Enable when you have your local docker set up running 
 class CloudtalkApplicationTests {
 
 	@Test
